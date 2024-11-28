dd4f930b78bff64c7b5dd09998ed07f49caf08ae
eliangwww <96296131+eliangwww@users.noreply.github.com>
2024-11-28 17:05:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -25,7 +25,7 @@ let 我的优选 = [
 ] //格式127.0.0.1:443#US@notls或[2606:4700:3030:0:4563:5696:a36f:cdc5]:2096#US，如果#US不填则使用统一名称，如果@notls不填则默认使用TLS，每行一个，如果不填任何节点会生成一个默认自身域名的小黄云节点
 let 我的优选TXT ='https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/data/ipss.txt?token=12123' //优选TXT路径，表达格式与上述相同，使用TXT时脚本内部填写的节点无效，二选一
 // 假设远程文件URL
-let txtFileUrl = 'https://example.com/my-preferred-nodes.txt';  // 替换为实际的远程文件URL
+let txtFileUrl = 'https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/data/ipss.txt?token=12123';  // 替换为实际的远程文件URL
 
 // 用来存储分类结果
 let 分类结果 = {};
