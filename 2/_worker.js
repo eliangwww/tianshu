45b2797b1cb73166c8ff2ade813feaa73c102cf8
eliangwww <96296131+eliangwww@users.noreply.github.com>
2024-11-29 15:49:20 +0800

    Update _worker.js

--- 2/_worker.js
+++ 2/_worker.js
@@ -23,8 +23,10 @@ let 嘲讽语 = "哎呀你找到了我，但是我就是不给你看，气不气
 let 我的优选 = [
   'www.visa.com',
 ] //格式127.0.0.1:443#US@notls或[2606:4700:3030:0:4563:5696:a36f:cdc5]:2096#US，如果#US不填则使用统一名称，如果@notls不填则默认使用TLS，每行一个，如果不填任何节点会生成一个默认自身域名的小黄云节点
-let 我的优选TXT ='https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/data/cfip.txt?token=12123' //优选TXT路径，表达格式与上述相同，使用TXT时脚本内部填写的节点无效，二选一
-
+let 我的优选TXT = ['https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/data/ips.txt?token=12123',
+              'https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/data/cfip.txt?token=12123'
+              
+              ]//优选TXT路径，表达格式与上述相同，使用TXT时脚本内部填写的节点无效，二选一
 let 启用反代功能 = true //选择是否启用反代功能【总开关】，false，true，现在你可以自由的选择是否启用反代功能了
 let 反代IP = 'ts.hpc.tw' //反代IP或域名，反代IP端口一般情况下不用填写，如果你非要用非标反代的话，可以填'ts.hpc.tw:443'这样
 
@@ -48,9 +50,11 @@ export default {
     const url = new URL(访问请求.url);
     if (!读取我的请求标头 || 读取我的请求标头 !== 'websocket') {
       if (我的优选TXT) {
-        const 读取优选文本 = await fetch(我的优选TXT);
-        const 转换优选文本 = await 读取优选文本.text();
-        const 优选节点 = 转换优选文本.split('\n').map(line => line.trim()).filter(line => line);
+        const 读取优选文本 = await Promise.all(我的优选TXT.map(url => fetch(url).then(response => response.text())));
+        const 转换优选文本 = 读取优选文本.flat();
+        const 优选节点 = 转换优选文本
+    .map(line => line.split('\n').map(line => line.trim()).filter(line => line)) // Split and clean each line
+    .flat();
         我的优选 = 优选节点 || 我的优选
       }
       switch (url.pathname) {
